99ef4834a1951c1000e672938f6b4fc0daaa28a9
Samir Merchant <fanged.parakeet@gmail.com>
2014-10-28 18:38:58 +0530

    catch-all node server function plus online/offline handlers added

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendfile(__dirname + '/index.html');
+app.get(/\w*/, function(req, res){
+	(req.url == '') ? res.sendfile(__dirname + '/index.html') : res.sendfile(__dirname + req.url);
 });
+
 
 io.on('connection', function(socket){
   socket.on('chat message', function(content){
